baa30d365eea0df0d4109c3a3e31ddb007ec4128
Michelle Cali <mctexx@gmail.com>
2016-08-31 11:37:56 -0500

    working a little better: cd ..
    /end

--- OOP/global.js
+++ OOP/global.js
@@ -1,7 +1,7 @@
 
 function Field(fieldName) {
 	this.fieldName = fieldName;
-	this.options = [];
+	// this.options = [];
 	this.images = [];
 	// this.addOptions = function(this.optionsArray, options)
 	// this.addOptions = function(options, fieldDocID){
@@ -21,18 +21,27 @@ Field.prototype = {
 	addOptions:function(optionsToAdd, fieldDocID){
 		this.fieldDocID = document.getElementById(fieldDocID);
 		for (i = 0; i < optionsToAdd.length; i++) {
-			this.options.push(optionsToAdd[i]);
+			// this.options.push(optionsToAdd[i]);
 			this.option = document.createElement("option");
-			this.option.text = optionsToAdd.key[i];
+			this.option.text = optionsToAdd[i];
 			this.fieldDocID.add(this.option);
 		}
 	}
+	// addOptions:function(optionsToAdd, fieldDocID){
+	// 	this.fieldDocID = document.getElementById(fieldDocID);
+	// 	for (i = 0; i < optionsToAdd.length; i++) {
+	// 		this.options.push(optionsToAdd[i]);
+	// 		this.option = document.createElement("option");
+	// 		this.option.text = optionsToAdd[i];
+	// 		this.fieldDocID.add(this.option);
+	// 	}
+	// }
 }
 
 window.addEventListener("load", function(){
 	// var quoteField = document.getElementById('quote');
 	var quoteObject = new Field("quote");
-	quoteObject.addOptions([Blue: "Yes: Blank", Red: "Yes: Prefilled (All Quotes)", Purple: "Yes: Prefilled (1/2 Verses)", Orange: "No: 3 Blank Lines"], "quote");
+	quoteObject.addOptions(["Yes: Blank", "Yes: Prefilled (All Quotes)", "Yes: Prefilled (1/2 Verses)", 	"No: 3 Blank Lines"], "quote");
 	// quoteObject = new Choice(["Yes: Blank", "Yes: Prefilled (All Quotes)", "Yes: Prefilled (1/2 Verses)", "No: 3 Blank Lines"], "quote", ["images/hughes/quote/yesBlank.png", "images/hughes/quote/yesAllQuotes.png", "images/hughes/quote/yesHalf.png", "images/hughes/quote/noLines.png"], "dashQuote", 0);
 	debugger;
 
