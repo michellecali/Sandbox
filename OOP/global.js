25717d5a9cda7d5171bef77b7c90ef2df48ba074
Michelle Cali <mctexx@gmail.com>
2016-08-31 12:07:47 -0500

    doesn't work

--- OOP/global.js
+++ OOP/global.js
@@ -1,47 +1,34 @@
 
-function Field(fieldName) {
+function Field(fieldName, fieldDocID, optionsToAdd) {
 	this.fieldName = fieldName;
-	// this.options = [];
-	this.images = [];
-	// this.addOptions = function(this.optionsArray, options)
-	// this.addOptions = function(options, fieldDocID){
-	// 	var fieldDocID = document.getElementById(fieldDocID);
-	// 	for (i = 0; i < this.options.length; i++){
-	// 		var option = document.createElement("option");
-	// 		option.text = i;
-	// 		fieldDocId.add(option);
-		}
-	// }
-	// this.images = [].push(images);
-	// this.imgDocID = document.getElementById(imgDocID);
-	// this.defaultIndex = defaultIndex;
-// }
-Field.prototype = {
-	constructor: Field,
-	addOptions:function(optionsToAdd, fieldDocID){
-		this.fieldDocID = document.getElementById(fieldDocID);
-		for (i = 0; i < optionsToAdd.length; i++) {
-			// this.options.push(optionsToAdd[i]);
+	this.fieldDocID = document.getElementById(fieldDocID);
+	this.optionsToAdd = optionsToAdd;
+	this.addOptions = function(){
+		this.optionsToAdd.forEach(function(opt){
 			this.option = document.createElement("option");
-			this.option.text = optionsToAdd[i];
+			this.option.text = opt;
 			this.fieldDocID.add(this.option);
-		}
+		});
 	}
-	// addOptions:function(optionsToAdd, fieldDocID){
-	// 	this.fieldDocID = document.getElementById(fieldDocID);
-	// 	for (i = 0; i < optionsToAdd.length; i++) {
-	// 		this.options.push(optionsToAdd[i]);
-	// 		this.option = document.createElement("option");
-	// 		this.option.text = optionsToAdd[i];
-	// 		this.fieldDocID.add(this.option);
-	// 	}
-	// }
 }
+
+
+// Field.prototype = {
+// 	constructor: Field,
+// 	addOptions:function(optionsToAdd, fieldDocID){
+// 		this.fieldDocID = document.getElementById(fieldDocID);
+// 		for (i = 0; i < optionsToAdd.length; i++) {
+// 			this.option = document.createElement("option");
+// 			this.option.text = optionsToAdd[i];
+// 			this.fieldDocID.add(this.option);
+// 		}
+// 	}
+// }
 
 window.addEventListener("load", function(){
 	// var quoteField = document.getElementById('quote');
-	var quoteObject = new Field("quote");
-	quoteObject.addOptions(["Yes: Blank", "Yes: Prefilled (All Quotes)", "Yes: Prefilled (1/2 Verses)", 	"No: 3 Blank Lines"], "quote");
+	var quoteObject = new Field("quote", "quote", ["Yes: Blank", "Yes: Prefilled (All Quotes)", "Yes: Prefilled (1/2 Verses)", "No: 3 Blank Lines"]);
+	// quoteObject.addOptions(["Yes: Blank", "Yes: Prefilled (All Quotes)", "Yes: Prefilled (1/2 Verses)", "No: 3 Blank Lines"], "quote");
 	// quoteObject = new Choice(["Yes: Blank", "Yes: Prefilled (All Quotes)", "Yes: Prefilled (1/2 Verses)", "No: 3 Blank Lines"], "quote", ["images/hughes/quote/yesBlank.png", "images/hughes/quote/yesAllQuotes.png", "images/hughes/quote/yesHalf.png", "images/hughes/quote/noLines.png"], "dashQuote", 0);
 	debugger;
 
