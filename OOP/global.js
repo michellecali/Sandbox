cb0d7c569bfac9c892137834fc6a8d90635ff8f7
Michelle Cali <mctexx@gmail.com>
2016-09-02 13:52:14 -0500

    it works, yo

--- OOP/global.js
+++ OOP/global.js
@@ -1,9 +1,8 @@
-
-var dictionary = {
-	"foo1": "images/hughes/quote/yesBlank.png",
-	"foo2": "images/hughes/quote/yesAllQuotes.png",
-	"foo3": "images/hughes/dailyHeaderLeft/bullets.png",
-	"foo4": "images/hughes/dailyHeaderLeft/checkboxes.png",
+var hughesQuote = {
+	"Yes: Blank": "images/hughes/quote/yesBlank.png",
+	"Yes: Quote": "images/hughes/quote/yesAllQuotes.png",
+	"Whatever": "images/hughes/dailyHeaderLeft/bullets.png",
+	"Ditto": "images/hughes/dailyHeaderLeft/checkboxes.png",
 }
 
 function Field(fieldName, fieldDocID, imageDocID) {
@@ -13,47 +12,29 @@ function Field(fieldName, fieldDocID, imageDocID) {
 	this.imageDocID = document.getElementById(imageDocID);
 	this.imageOptions = [];
 }
+function getArray(array) {
+	var valueTextArray = {};
+	for (var i = 0; i < array.length; i++) {
+		valueTextArray[array[i].value] = array[i].text.toString();
+	}
+	return valueTextArray;
+}
 
 Field.prototype = {
 	constructor: Field,
 	addOptions:function(optionsToAdd){	
 		this.fieldOptions = (optionsToAdd).keys();
 	}
-	// setDefault:function(defaultIndex){		
-	// 	this.imageDocID.src = this.fieldOptions[defaultIndex];
-	// 	this.fieldDocID.options.selectedIndex = defaultIndex;
-	// 	}
 }
-
 window.addEventListener("load", function(){
-	// var quoteField = document.getElementById('quote');
-	// var quoteObject = new Field("quote", "quote", "dashQuote");
-	// quoteObject.addOptions({
-	// 	"Yes: Blank": "images/hughes/quote/yesBlank.png",
-	// 	"Yes: Prefilled (All Quotes)": "images/hughes/quote/yesAllQuotes.png",
-	// 	"Yes: Prefilled (1/2 Verses)": "images/hughes/quote/yesHalf.png",
-	// 	"No: 3 Blank Lines": "images/hughes/quote/noLines.png"
-	// });
 	var fieldOptions = document.getElementById("consistentID").querySelectorAll("option");
-	function getArray(array) {
-		var textValueArray = {};
-		for (var i = 0; i < array.length; i++) {
-			textValueArray[array[i].text] = array[i].value;
-			// valuesArray.push(array[i].value);
-		}
-		return textValueArray;
-	};
-	var valuesOnly = getArray(fieldOptions);
-	debugger;
-
-	var field = document.getElementById("consistentID").children[2];
-	// quoteObject.setDefault("Yes: Blank");
-
-	
+	var valueTextArray = getArray(fieldOptions);
+	var field = document.getElementById("consistentID").children[2];	
 	var quoteImg = document.getElementById('dashQuote');
-	field.addEventListener("change", function(){		
+	field.addEventListener("change", function(){	
 		var i = field.options[field.selectedIndex].value;
-		dashQuote.src = dictionary[i];
+		var j = valueTextArray[i];
+		dashQuote.src = hughesQuote[j];
 	});
 });
 
